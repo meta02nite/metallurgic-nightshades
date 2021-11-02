a7d0099a7e8a7516b9f679b7fffdf00c251de8ef
metanite <meta02nite@gmail.com>
2021-11-02 01:29:49 -0600

    Hardware.inc updates

--- src/main.s
+++ src/main.s
@@ -1,14 +1,11 @@
+include "hardware.inc"
+
 ; Nintendo Logo wobble test
 section "HEADER", ROM0[$0100]
     nop
     jp wobble_main
     ; ROM header
-    db $CE,$ED,$66,$66,$CC,$0D,$00,$0B
-	db $03,$73,$00,$83,$00,$0C,$00,$0D
-	db $00,$08,$11,$1F,$88,$89,$00,$0E
-	db $DC,$CC,$6E,$E6,$DD,$DD,$D9,$99
-	db $BB,$BB,$67,$63,$6E,$0E,$EC,$CC
-    db $DD,$DC,$99,$9F,$BB,$B9,$33,$3E
+    NINTENDO_LOGO
     db "MetaGB", $00
     ; Entry
 section "MetaGB", ROM0[$0150]
@@ -22,6 +19,7 @@ wobble_loop:
     ; B will be the scanline that
     ; we want to transform
     ld b, $00
+    jr wobble_loop
 .inner_loop:
     ; Load current scanline position
     ; and compare to B
@@ -66,3 +64,5 @@ section "WOBBLE_DATA", ROM0[$2000]
 	db $02,$02,$02,$02,$01,$01,$00,$00
 	db $00,$00,$FF,$FF,$FE,$FE,$FE,$FE
 	db $FE,$FE,$FE,$FE,$FF,$FF,$00,$00
+
+; vim: filetype=rgbds
